9c4c24017f2980797ad979fda3310de622ef3e42
MatheusBM <matheusmbm13@gmail.com>
2021-10-08 12:03:29 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Box from './Components/Box'
+
+function App() {
+  return (
+    <div className="App">
+      <Box Name="Mesa 01" ></Box>
+      <Box Name="Mesa 02"></Box>
+    </div>
+  );
+}
+
+export default App;
