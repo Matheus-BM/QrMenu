c4b25097ff2fb616c16e4d9ad20d8f7991ab9499
Macedo <pedromacedo113@hotmail.com>
2021-11-12 12:32:41 -0300

    alteração modal/categorias

--- src/Components/ItemMenu/ItemMenu.js
+++ src/Components/ItemMenu/ItemMenu.js
@@ -1,27 +1,27 @@
-import React,{useContext} from 'react'
-import { listMenuItems, } from '../../ListMenuItems'
-import './ItemMenu.css'
-import { ContextlistaDePedidos } from '../../ListMenuItems'
+import React, { useContext } from "react";
+import { listMenuItems } from "../../ListMenuItems";
+import "./ItemMenu.css";
+import { ContextlistaDePedidos } from "../../ListMenuItems";
 
-function ItemMenu({nome, preco,id}) {
+function ItemMenu({ nome, preco, id, descricao }) {
+  const { addPedido } = useContext(ContextlistaDePedidos);
 
-    const {addPedido} = useContext(ContextlistaDePedidos);
+  function addPedidos(id) {
+    addPedido(listMenuItems.find((item) => item.id === id));
+  }
 
-    function addPedidos (id){      
-        
-        addPedido( listMenuItems.find(item => item.id === id));
-       
-        
-    }
-
-
-    return (
-        <div className="item">
-            <h3>{nome}</h3>
-            <p>{preco}</p>
-            <button className="btn add" onClick={() => addPedidos(id)} >+</button>
-        </div>
-    )
+  return (
+    <div className="item">
+      <div className="item-header">
+        <h3>{nome}</h3>
+        <p>{preco}</p>
+        <button className="btn add" onClick={() => addPedidos(id)}>
+          +
+        </button>
+      </div>
+      <p id="item-desc">{descricao}</p>
+    </div>
+  );
 }
 
-export default ItemMenu
+export default ItemMenu;
--- src/Components/modalPedido/ModalPedido.js
+++ src/Components/modalPedido/ModalPedido.js
@@ -1,19 +1,36 @@
-import React from 'react'
-import './ModalPedido.css'
-import { useContext } from 'react'
-import { ContextlistaDePedidos } from '../../ListMenuItems'
+import React from "react";
+import "./ModalPedido.css";
+import { useContext } from "react";
+import { ContextlistaDePedidos } from "../../ListMenuItems";
 
-const ModalPedido = ( ) => {
+const ModalPedido = () => {
+  const { pedidos, deletePedido } = useContext(ContextlistaDePedidos);
 
-    const {pedidos,deletePedido} = useContext(ContextlistaDePedidos)
+  const MenuActive = () => {
+    let arrowRot = document.getElementById("active");
+    arrowRot.classList.toggle("arrow-active");
+    let pedidosList = document.getElementById("pedidos-list");
+    pedidosList.classList.toggle("modal-active");
+  };
 
+  return (
+    <div id="pedidos-list" className="Modal">
+      <div className="box-modal">
+        <i id="active" className="fas fa-chevron-down" onClick={MenuActive}></i>
+        <h2>Pedidos</h2>
+      </div>
+      <ul>
+        {pedidos.map((item, i) => (
+          <li key={i++}>
+            {item.nome} <p>{item.qt}</p>{" "}
+            <button className="btn remove" onClick={() => deletePedido(item)}>
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    return (
-        <div className="Modal">
-            <h2>Pedidos</h2>
-            <ul>{pedidos.map((item,i)=> <li key = {i++}>{item.nome} <p>{item.qt}</p> <button className="btn remove" onClick = {( ) => deletePedido(item)}>x</button></li>)}</ul>
-        </div>
-    )
-}
-
-export default ModalPedido
+export default ModalPedido;
