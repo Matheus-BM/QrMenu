c7aaccf506021001d1514aa96a5773a5fdb23e16
MatheusBM <matheusmbm13@gmail.com>
2021-11-12 18:54:01 -0300

    Add backend

--- src/Components/index.js
+++
@@ -1,5 +0,0 @@
-import CategoriaMenu from './CategoriasMenu/CategoriaMenu';
-import Title from './Title/Title';
-import ModalPedido from './modalPedido/ModalPedido';
-
-export {CategoriaMenu, Title, ModalPedido};
