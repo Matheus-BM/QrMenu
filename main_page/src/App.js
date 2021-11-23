a51ead0180a469a4ee57faa2c83c5ad11f28197f
MatheusBM <matheusmbm13@gmail.com>
2021-11-23 17:48:20 -0300

    merge menu and main page

--- main_page/src/App.js
+++ main_page/src/App.js
@@ -7,7 +7,8 @@ import PublicRoute from "./utils/PublicRoute";
 import PrivateRoute from "./utils/PrivateRoute";
 import QrCode from "./pages/QrCode/QrCode"
 import PersonalData from "./pages/PersonalData/PersonalData"
-
+import  PedidosProvider  from './ListMenuItems';
+import {CategoriaMenu, ModalPedido, Title} from './components/index'
 
 function App() {
 
@@ -19,6 +20,20 @@ function App() {
         <Route exact path="/Dashboard" element={ <PrivateRoute> <MenuCreate /> </PrivateRoute>} />
         <Route path="/Dashboard/QrCode" element={<PrivateRoute> <QrCode/> </PrivateRoute>}/>
         <Route path="/Dashboard/DadosPessoais" element={<PrivateRoute><PersonalData/></PrivateRoute>} />
+        <Route exact path="/:nomeRestaurante" element={
+            <div className="fundo">
+            <PedidosProvider>
+              <div className="container">
+               <Title value = "MENU"/>
+                <CategoriaMenu/>
+                <ModalPedido/>
+              </div>
+            </PedidosProvider>
+            
+            </div>
+          } 
+        /> 
+     
       </Routes>
     </Router>
   );
